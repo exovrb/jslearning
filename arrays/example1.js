210d1d0c916c66facd00151489b9f2f87a52867b
exovrb <exovrb@gmail.com>
2025-03-16 21:13:55 +0300

    new learning

---
+++ arrays/example1.js
@@ -0,0 +1,7 @@
+const myArray = [1, 2, 3]
+console.log(myArray)
+
+const myArray2 = new Array(1, 2, 3)
+console.log(myArray2)
+
+console.log(myArray.length)
---
+++ instruction/expressionInstruction.js
@@ -0,0 +1,13 @@
+// Выражение может быть инструкцией
+
+'abc';
+
+a = a + 3;
+
+c = a + b;
+
+d = 'Good' + 'Evening';
+
+myFunction(c, d);
+
+console.log('Hey');
