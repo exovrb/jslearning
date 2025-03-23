e09089b903804ad01708a39f5bb760f48b8d821b
exovrb <exovrb@gmail.com>
2025-03-23 18:11:32 +0300

    new learning

--- modules/example/moduleTwo.mjs
+++ modules/example/moduleTwo.mjs
@@ -1,12 +1,10 @@
 // import printMyName from './moduleOne.js' - импорт одного элемента
 
-import {
-    myName,
-    sum
-} from './moduleOne.mjs'
+import { myName as printMyName,sum } from './moduleOne.mjs'
 // имопрт нескольких элементов
+// as позволяет переименовывать
 
-myName()
+printMyName()
 
 const res = sum(10, 5)
 console.log(res)
